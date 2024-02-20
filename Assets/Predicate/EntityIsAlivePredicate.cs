81edfff466417f12506c9c87fa84a9d054bc2819
AlejandroMacazaga <alejandrolopezmacazaga@gmail.com>
2024-02-20 20:26:32 +0100

    added state machine pattern

---
+++ Assets/Predicate/EntityIsAlivePredicate.cs
@@ -0,0 +1,20 @@
+﻿using Command;
+
+namespace Predicate
+{
+    public class EntityIsAlivePredicate : IPredicate
+    {
+        private readonly IEntity _entity;
+        
+        public EntityIsAlivePredicate(IEntity entity)
+        {
+            this._entity = entity;
+        }
+        
+        public bool Evaluate()
+        {
+            // Evaluate if the entity is alive
+            return true;
+        }
+    }
+}
---
+++ Assets/State/EntityState.cs
@@ -0,0 +1,39 @@
+﻿using Command;
+
+namespace State
+{
+    public abstract class EntityState : IState
+    {
+        protected readonly IEntity Entity;
+        
+        protected EntityState(IEntity entity)
+        {
+            this.Entity = entity;
+        }
+
+        public virtual void OnEnter()
+        {
+            
+        }
+
+        public virtual void OnExit()
+        {
+            
+        }
+
+        public virtual void OnUpdate()
+        {
+            
+        }
+
+        public virtual void OnFixedUpdate()
+        {
+            
+        }
+        
+        public static T Create<T>(IEntity entity) where T : EntityState
+        {
+            return (T)System.Activator.CreateInstance(typeof(T), entity);
+        }
+    }
+}
