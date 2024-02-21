b9922ee609fcfa2e2cc35e140e76e2975a7732a7
AlejandroMacazaga <alejandrolopezmacazaga@gmail.com>
2024-02-21 19:31:41 +0100

    Added visitor pattern

--- Assets/State/StateMachine.cs
+++ Assets/State/StateMachine.cs
@@ -7,10 +7,11 @@ namespace State
 {
     public class StateMachine
     {
-        private StateNode current;
-        private Dictionary<Type, StateNode> nodes = new();
-        private HashSet<ITransition> anyTransitions = new();
+        private StateNode _current;
+        private readonly Dictionary<Type, StateNode> _nodes = new();
+        private readonly HashSet<ITransition> _anyTransitions = new();
 
+        // Called on the update loop in the monobehaviour
         public void Update()
         {
             var transition = GetTransition();
@@ -19,69 +20,78 @@ namespace State
                 ChangeState(transition.To);
             }
             
-            current.State?.OnUpdate();
+            _current.State?.OnUpdate();
         }
         
+        // Called on the fixed update loop in the monobehaviour
         public void FixedUpdate()
         {
-            current.State?.OnFixedUpdate();
+            _current.State?.OnFixedUpdate();
         }
 
+        // Set the initial state
         public void SetState(IState state)
         {
-            current = nodes[state.GetType()];
-            current.State?.OnEnter();
+            _current = _nodes[state.GetType()];
+            _current.State?.OnEnter();
         }
         
+        // Change the current state to the new state
         void ChangeState(IState state)
         {
-            if (state == current.State) return;
+            if (state == _current.State) return;
             
-            var previousState = current.State;
-            var nextState = nodes[state.GetType()].State;
+            var previousState = _current.State;
+            var nextState = _nodes[state.GetType()].State;
             
             previousState?.OnExit();
             nextState?.OnEnter();
-            current = nodes[state.GetType()];
+            _current = _nodes[state.GetType()];
         }
         
+        // Get the transition to the next state
         ITransition GetTransition()
         {
-            foreach (var transition in anyTransitions)
+            foreach (var transition in _anyTransitions)
                 if (transition.Condition.Evaluate())
                     return transition;
             
-            foreach (var transition in current.Transitions)
+            foreach (var transition in _current.Transitions)
                 if (transition.Condition.Evaluate())
                     return transition;
 
             return null;
         }
         
+        // Add a transition from one state to another
         public void AddTransition(IState from, IState to, IPredicate condition)
         {
             GetOrAddNode(from).AddTransition(GetOrAddNode(to).State, condition);
         }
         
+        // Add a transition from any state to another
         public void AddAnyTransition(IState to, IPredicate condition)
         {
-            anyTransitions.Add(new Transition.Transition(GetOrAddNode(to).State, condition));
+            _anyTransitions.Add(new Transition.Transition(GetOrAddNode(to).State, condition));
         }
         
+        // Get or add a node to the state machine
         StateNode GetOrAddNode(IState state)
         {
-            var node = nodes.GetValueOrDefault(state.GetType());
+            var node = _nodes.GetValueOrDefault(state.GetType());
             
             if (node == null)
             {
                 node = new StateNode(state);
-                nodes.Add(state.GetType(), node);
+                _nodes.Add(state.GetType(), node);
             }
 
             return node;
 
         }
         
+        // A node in the state machine
+        // Contains the state and the transitions from that state
         class StateNode
         {
             public IState State { get; }
---
+++ Assets/Visitor/PowerUp.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Visitor
+{
+    [CreateAssetMenu(fileName = "PowerUp", menuName = "PowerUp")]
+    public class PowerUp : ScriptableObject, IVisitor
+    {
+        public int HealthBonus = 10;
+        
+        public void Visit(HealthComponent healthComponent)
+        {
+            healthComponent.AddHealth(HealthBonus);
+            Debug.Log("PowerUp.Visit");
+        }
+    }
+}
